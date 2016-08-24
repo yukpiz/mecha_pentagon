5636b3932ddce4dad412aa22aea9a1bbf671d3b8
yukpiz <yukpiz@gmail.com>
2016-08-24 16:53:55 +0900

    BOTのコアを別モジュールに分離

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 var restify = require('restify');
-var builder = require('botbuilder');
 var fs = require('fs');
 
 var https_options = {
@@ -7,49 +6,10 @@ var https_options = {
     certificate: fs.readFileSync('/etc/letsencrypt/live/yukpiz.me/cert.pem')
 };
 
-var yaml = require('yamljs');
-config = yaml.load('config.yml');
-
-var appId = config.microsoft.appId;
-var appSecret = config.microsoft.appSecret;
-
 var server = restify.createServer(https_options);
 server.listen(process.env.port || process.env.PORT || 8081, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-var connector = new builder.ChatConnector({
-    appId: appId,
-    appPassword: appSecret
-});
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-bot.on('conversationUpdate', function (message) {
-    // Add Members of Group.
-    if (message.address.conversation.isGroup && message.membersAdded) {
-        message.membersAdded.forEach(function(identity) {
-            if (identity.id === message.address.bot.id) {
-                var reply = new builder.Message()
-                    .address(message.address)
-                    .text(
-                            "うまるちゃんが参加しました！<br/>" +
-                            "皆さんよろしくおねがいします(｡･･｡)"
-                         );
-                bot.send(reply);
-            }
-        });
-    }
-});
-
-bot.dialog('/', function (session) {
-    console.log(session.message.address);
-    console.log(session.message.address.user);
-    console.log(session.message.address.conversation);
-    console.log(session.message.address.bot);
-    var parse = require('../core/parse');
-    var reply_msg = parse.reply(session, builder, function(msg) {
-        console.log(msg);
-        session.send(msg);
-    });
-});
+var bot = require('../core/bot');
+server.post('/api/messages', bot.connector.listen());
