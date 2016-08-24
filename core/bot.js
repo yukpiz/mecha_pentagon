104c8215f2644563516b73cc2d614a0834198311
yukpiz <yukpiz@gmail.com>
2016-08-24 13:35:38 +0900

    サーバーとBOTコアを分離

--- core/bot.js
+++ core/bot.js
@@ -1,11 +1,5 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var fs = require('fs');
-
-var https_options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/yukpiz.me/privkey.pem'),
-    certificate: fs.readFileSync('/etc/letsencrypt/live/yukpiz.me/cert.pem')
-};
 
 var yaml = require('yamljs');
 config = yaml.load('config.yml');
@@ -13,17 +7,11 @@ config = yaml.load('config.yml');
 var appId = config.microsoft.appId;
 var appSecret = config.microsoft.appSecret;
 
-var server = restify.createServer(https_options);
-server.listen(process.env.port || process.env.PORT || 8081, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-
-var connector = new builder.ChatConnector({
+exports.connector = new builder.ChatConnector({
     appId: appId,
     appPassword: appSecret
 });
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
 
 bot.on('conversationUpdate', function (message) {
     // Add Members of Group.
