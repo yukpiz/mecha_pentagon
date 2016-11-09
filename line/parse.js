dd7d2558cafb5f219ce674fb086e8852ddca6331
yukpiz <yukpiz@gmail.com>
2016-11-09 18:35:18 +0900

    Updated.

--- line/parse.js
+++ line/parse.js
@@ -9,12 +9,44 @@ exports.reply = function(e) {
 		'人が深淵を覗く時、深淵もまた人を覗いているのだ！'
 		: reply_message;
 
-	if (!reply_message) {
-		return;
-	}
+	var MeCab = new require('mecab-async');
+	var mecab = new MeCab();
+	mecab.parse(message, function(err, res) {
+		console.log(res);
+        var mongo = require('../core/mongodb');
+		db = mongo.initDb();
+		db.open(function() {
+            var collection = db.collection('megumin_analyzer');
+			var words = new Array();
+			res.forEach(function(a) {
+				if (a[1] == '名詞' || a[1] == '感動詞') {
+					words.push(a[0]);
+					collection.find({ word: a[0] }).count(function(err, c) {
+						if (c != 0) return;
+						collection.insert({
+							word: a[0],
+							reply: [],
+						}, function() {
+							console.log('Saved: ' + a[0]);
+							db.close();
+						});
+					});
+				}
+			});
 
-    var yaml = require('yamljs');
-    var config = yaml.load('config.yml');
+			var random_word = words[Math.floor(Math.random() * words.length)];
+			collection.findOne({ word: random_word }, function(err, doc) {
+				if (doc.reply.length == 0) return;
+				var random_reply = doc.reply[Math.floor(Math.random() * doc.reply.length)];
+				console.log(random_reply);
+			});
+		});
+	});
+
+	if (!reply_message) return;
+
+	var yaml = require('yamljs');
+	var config = yaml.load('config.yml');
 
 	var request = require('request');
 	var headers = {
