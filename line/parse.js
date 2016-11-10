be115faa0a0f65755865b4c695805e14fff5114c
yukpiz <yukpiz@gmail.com>
2016-11-10 17:12:14 +0900

    Updated.

--- line/parse.js
+++ line/parse.js
@@ -10,6 +10,45 @@ exports.reply = function(e) {
 		db = mongo.initDb();
 		db.open(function() {
 			var collection = db.collection('megumin_analyzer');
+			m = message.match(/[v|V]ersion/);
+			if (m) {
+				collection.find({}).count(function(err, c) {
+					reply_message =
+						'AI CODE: mecha_pentagon::megumin_analyzer\n' +
+						'  https://github.com/yukpiz/mecha_pentagon\n' +
+						'  今の単語数は ' + c + ' 件です！';
+					send(e.replyToken, reply_message);
+				});
+				return;
+			}
+
+			m = message.match(/^\!add\s[\"|\'](.*)[\"|\']\s[\"|\'](.*)[\"|\']/);
+			if (m) {
+				collection.find({word:m[1]}).count(function(err, c) {
+					console.log(c);
+					if (c == 0) {
+						//Insert
+						collection.insert({word:m[1], reply:[m[2]]}, function() {
+							console.log('SUCCESS ADD COMMAND BY INSERT: ' + m[1] + ',' + m[2]);
+						});
+					} else {
+						//Update
+						collection.update({word:m[1]}, {$push:{reply:m[2]}}, function() {
+							console.log('SUCCESS ADD COMMAND BY UPDATE: ' + m[1] + ',' + m[2]);
+						});
+					}
+				});
+				return
+			}
+
+			m = message.match(/^\!remove\s[\"|\'](.*)[\"|\']/);
+			if (m) {
+				collection.remove({word:m[1]}, function(err) {
+					console.log('SUCCESS REMOVE COMMAND: ' + m[1]);
+				});
+				return
+			}
+
 			var words = new Array();
 			res.forEach(function(a) {
 				if (a[1] == '名詞' || a[1] == '感動詞') {
@@ -26,18 +65,6 @@ exports.reply = function(e) {
 					});
 				}
 			});
-
-			m = message.match(/[v|V]ersion/);
-			if (m) {
-				collection.find({}).count(function(err, c) {
-					reply_message =
-						'AI CODE: mecha_pentagon::megumin_analyzer\n' +
-						'  https://github.com/yukpiz/mecha_pentagon\n' +
-						'  今の単語数は ' + c + ' 件です！';
-					send(e.replyToken, reply_message);
-				});
-				return;
-			}
 
 			var random_word = words[Math.floor(Math.random() * words.length)];
 			collection.findOne({ word: random_word }, function(err, doc) {
